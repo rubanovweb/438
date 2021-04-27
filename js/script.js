2fa46f39d6a04caae2822d8eb7c2e7409920fee7
Александр <rubanov.web@gmail.com>
2021-04-27 15:22:28 +0300

    Решена Задача № 2

--- js/script.js
+++ js/script.js
@@ -1,33 +1,59 @@
+/**** Задача 2 ****/
+// Пользователь вводит длину оснований трапеции (a и b), а также высоту трапеции h.
+// Программа выводит сообщение: «Площадь трапеции будет равна <значение>». Площадь
+// вычисляется по формуле ((a + b) / 2) * h, где a, b - основания, h - высота.
+
+let a, b; //стороны трапеции
+let h; //высота трапеции
+let S; //искомая площадь трапеции
+
+let result = ""; //строка с результатом
+
+a = +prompt("Сторона a:");
+b = +prompt("Сторона b:");
+h = +prompt("Высота h:");
+
+if(a > 0 && b > 0 && h > 0) {
+  S = ((a + b) / 2) * h;
+  result = `Площадь трапеции со сторонами a = ${a}, b = ${b}\nи высотой h = ${h} равна ${S}.`
+}
+else {
+  result = "Неверные данные!!! Площадь невозможно посчитать!";
+}
+alert(result);
+
+
+
+/**** Задача 1 ****/
 // Программа хранит в двух переменных курс доллара и евро. В первой переменной у вас
 // хранится стоимость одного евро в рублях, во второй - стоимость одного доллара в рублях.
 // Вы спрашиваете у пользователя, сколько рублей он хочет сконвертировать, получаете это
 // число и считаете. Результат надо вывести на страницу с помощью alert.
+// let kursDollara = 74.8;
+// let kursEuro = 90.5;
+// let sumDollars = 0, sumEuro = 0;
 
-let kursDollara = 74.8;
-let kursEuro = 90.5;
-let sumDollars = 0, sumEuro = 0;
+// let rubles = prompt("Сумма в рублях:");
 
-let rubles = prompt("Сумма в рублях:");
+// if(!rubles && typeof rubles == "object") {
+//   alert("Вы отменили!!!");
+// }
+// else {
+//   if(rubles == "") {
+//     alert("Вы ничего не ввели!!!");
+//   }
+//   else {
+//     if(isNaN(+rubles)) {
+//       alert("Это не число!");
+//     }
+//     else {
+//       sumDollars = (rubles/kursDollara).toFixed(2);
+//       sumEuro = (rubles/kursEuro).toFixed(2);
 
-if(!rubles && typeof rubles == "object") {
-  alert("Вы отменили!!!");
-}
-else {
-  if(rubles == "") {
-    alert("Вы ничего не ввели!!!");
-  }
-  else {
-    if(isNaN(+rubles)) {
-      alert("Это не число!");
-    }
-    else {
-      sumDollars = (rubles/kursDollara).toFixed(2);
-      sumEuro = (rubles/kursEuro).toFixed(2);
-
-      alert(`Сумма в долларах = ${sumDollars}$, сумма в евро = ${sumEuro}\u20AC`);
-    }
-  }
-}
+//       alert(`Сумма в долларах = ${sumDollars}$, сумма в евро = ${sumEuro}\u20AC`);
+//     }
+//   }
+// }
 
 // let number = prompt("Введите число:"); //возвращает тип string или null
 
