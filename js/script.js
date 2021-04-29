0867bbfceeaf2b3f7af88eaadc3cdca66a647c83
Александр <rubanov.web@gmail.com>
2021-04-29 17:29:31 +0300

    Пример собственного объекта и DOM-объекта

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+/*** ОБЪЕКТЫ (начало) ***/
+// Литеральный способ создания объекта {}
+let button = {
+   text: "Купить",
+
+   position: "absolute",
+   top: "50%", 
+   left: "50%",
+
+   width: "350px",
+   height: "70px",
+   marginLeft: "-175px",
+   marginTop: "-35px",
+   padding: "10px 30px",
+
+   fontSize: "18px",
+   color: "#ffffff",
+   backgroundColor: "darkred",
+   boxShadow: "0 0 5px #ff0000",
+   borderRadius: "10px",
+
+   isBorder: false
+};
+
+let btn = document.createElement("button");
+
+for(let property in button)
+{
+   if(property == "text") {
+      btn.textContent = button[property];
+   }
+   else {
+      if(property == "isBorder") {
+         if(button[property]) {
+            btn.style.border = "2px solid darkred";
+         }
+         else {
+            btn.style.border = "none";
+         }
+      }
+      else {
+         btn.style[property] = button[property];
+      }
+   }
+}
+
+document.body.prepend(btn);
+
+// console.dir(btn.style.boxShadow);
+
+
+//string, number, boolean, Array, Object, Function
+
+
+/*** ОБЪЕКТЫ (конец) ***/
+
+
+
 // Примитивные типы данных
 //string "123"
 //number 12; 14.6
